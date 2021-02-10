6b95e610170a39b0f576aa0484cec138ca0ccdbf
liding <539561425@qq.com>
2021-02-10 21:08:50 +0800

    feat: banner部分

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import Header from './header';
 import styles from './index.less';
+import Banner from './banner';
 
 export default function IndexPage() {
   return (
     <div className={styles.title}>
       <Header />
+      <Banner />
     </div>
   );
 }
